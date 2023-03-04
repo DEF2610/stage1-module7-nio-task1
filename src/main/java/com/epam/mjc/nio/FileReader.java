525283aa248a201ed32a6242d1b676c1663ec82e
d <maksimukart@mail.com>
2023-03-04 17:40:04 +0100

    d

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,35 @@
 package com.epam.mjc.nio;
 
+import java.io.BufferedReader;
 import java.io.File;
-
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
 
 public class FileReader {
+    HashMap<String, String> lineMap = new HashMap<>();
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Path path = file.toPath();
+        try (BufferedReader reader = Files.newBufferedReader(path)) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                parse(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Profile profile = new Profile();
+        profile.setName(lineMap.get("Name:"));
+        profile.setEmail(lineMap.get("Email:"));
+        profile.setAge(Integer.parseInt(lineMap.get("Age:")));
+        profile.setPhone(Long.parseLong(lineMap.get("Phone:")));
+        return profile;
+    }
+
+    public void parse(String line) {
+        String[] lines = line.split(" ");
+        lineMap.put(lines[0], lines[1]);
     }
 }
